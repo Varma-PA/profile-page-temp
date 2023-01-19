280382893bf839ca57f627c6f1c56eff9b802f8c
Achyuth Varma <achyuth.varma1@gmail.com>
2023-01-19 13:45:47 -0500

    Completed with setting up base css and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/main.scss";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import router from "./router";
+import { Fragment, useState, useEffect } from "react";
+import HomePage from "./pages/Home";
+import Contact from "./pages/Contact";
+import Blogs from "./pages/Blogs";
+import ErrorPage from "./pages/ErrorPage";
+import { ThemeContext } from "./ThemeContext";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (toggle) setTheme("dark");
+    else setTheme("light");
+  }, [toggle]);
+
+  const changeTheme = () => {
+    setToggle((toggle) => !toggle);
+  };
+
   return (
-    <div>
-      <Header />
-      <main>
-        <RouterProvider router={router} />
-      </main>
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <Router>
+        <Fragment>
+          <Header callBack={changeTheme} />
+          <main className={`main-block main-${theme}`}>
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/blogs" element={<Blogs />}></Route>
+              <Route path="/404-not-found" element={<ErrorPage />}></Route>
+              <Route path="*" element={<ErrorPage />}></Route>
+            </Routes>
+            {/* <RouterProvider router={router} /> */}
+          </main>
+          <Footer />
+        </Fragment>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
